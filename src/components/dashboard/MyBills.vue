9892f01f54047257f09129d02d078ccaf109040b
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-12-14 19:23:54 +0500

    add dashboard , add seo

---
+++ src/components/dashboard/MyBills.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="start_title text-green">My Bills</h1>
+</template>
+
+<script>
+export default {
+  name: "MyBills"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/dashboard/ProfileComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="start_title text-green">Profile</h1>
+</template>
+
+<script>
+export default {
+  name: 'ProfileComponent'
+}
+</script>
+
+<style >
+.text-green {
+  color: var(--green) !important;
+}
+</style>
