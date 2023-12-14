cc42f3a021e6bd7b5b90b154fc7cb8f7c16162c9
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-12-14 09:03:05 +0500

    upd how to start

--- src/components/start/HowToStart.vue
+++ src/components/start/HowToStart.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="container d-flex center flex-column boxs_parent">
-    <div class="start_title mb-3">HOW TO START</div>
-    <div class="start_boxs">
-      <div class="start_box d-flex flex-column">
-        <img class="start_box_image" src="@/assets/images/home/start1.png" alt="" />
-        <div class="start_box_title">SIGN UP</div>
-        <div class="start_box_description">
-          Fill out the form and get ready to start mining within 1 minute.
+  <div class="start_parent">
+    <div class="container d-flex center flex-column boxs_parent">
+      <div class="start_title mb-3">HOW TO START</div>
+      <div class="start_boxs">
+        <div class="start_box d-flex flex-column">
+          <img class="start_box_image" src="@/assets/images/home/start1.png" alt="" />
+          <div class="start_box_title">SIGN UP</div>
+          <div class="start_box_description">
+            Fill out the form and get ready to start mining within 1 minute.
+          </div>
         </div>
-      </div>
-      <div class="start_box d-flex flex-column">
-        <img class="start_box_image" src="@/assets/images/home/start2.png" alt="" />
-        <div class="start_box_title">CHOOSE YOUR CONTRACTS</div>
-        <div class="start_box_description">
-            We provide you with profitable and risk-free cloud mining contracts. </div>
-      </div>
-      <div class="start_box d-flex flex-column">
-        <img class="start_box_image" src="@/assets/images/home/start3.png" alt="" />
-        <div class="start_box_title">start learning</div>
-        <div class="start_box_description">
-            After purchasing the contract, daily profits will be promptly deposited into your account.  </div>
+        <div class="start_box d-flex flex-column">
+          <img class="start_box_image" src="@/assets/images/home/start2.png" alt="" />
+          <div class="start_box_title">CHOOSE YOUR CONTRACTS</div>
+          <div class="start_box_description">
+            We provide you with profitable and risk-free cloud mining contracts.
+          </div>
+        </div>
+        <div class="start_box d-flex flex-column">
+          <img class="start_box_image" src="@/assets/images/home/start3.png" alt="" />
+          <div class="start_box_title">start learning</div>
+          <div class="start_box_description">
+            After purchasing the contract, daily profits will be promptly deposited into your
+            account.
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -31,7 +36,7 @@ export default {}
 
 <style scoped>
 .container {
-    max-width: 1440px;
-    margin: 0 auto;
+  max-width: 1440px;
+  margin: 0 auto;
 }
 </style>
