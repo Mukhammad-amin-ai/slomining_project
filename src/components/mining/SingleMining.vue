347058abf3b9c9df024fda62926d2d59cecb8caa
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-12-15 12:09:35 +0500

    add modal

--- src/components/mining/SingleMining.vue
+++ src/components/mining/SingleMining.vue
@@ -1,58 +1,96 @@
 <template>
-  <div class="mining_item_card">
-    <div class="item_card_left">
-      <img :src="item.image" :alt="item.name" class="item_card_image" />
-      <div class="item_card_name">
-        {{ item.name }}
+  <div>
+    <div class="mining_item_card">
+      <div class="item_card_left">
+        <img :src="item.image" :alt="item.name" class="item_card_image" />
+        <div class="item_card_name">
+          {{ item.name }}
+        </div>
+        <div class="item_card_volume">
+          Volume <span class="volume_value text-green">{{ item.volume }}%</span>
+        </div>
+        <div class="item_card_sold">(Sold out at 100%)</div>
       </div>
-      <div class="item_card_volume">
-        Volume <span class="volume_value text-green">{{ item.volume }}%</span>
+      <div class="item_card_right">
+        <div class="item_right_heading">
+          <div class="item_term">
+            <div class="term_title">Contract Term</div>
+            <div class="term_content">
+              {{ item.contract_term }}
+            </div>
+          </div>
+          <div class="item_term">
+            <div class="term_title">Contract Price</div>
+            <div class="term_content">
+              {{ item.contract_price }}
+            </div>
+          </div>
+        </div>
+        <div class="item_right_body">
+          <div class="item_term">
+            <div class="term_title">Daily Rewards</div>
+            <div class="term_content">
+              {{ item.daily_rewards }}
+            </div>
+          </div>
+        </div>
+        <div class="item_right_footer d-flex justify-content-start">
+          <div @click="showModal = true">
+            <ButtonComponent text="Order Now" class="default_black" />
+          </div>
+          <router-link :to="'/product-detail/' + item.id">
+            <ButtonComponent text="See more" class="default_white ml-1" />
+          </router-link>
+        </div>
       </div>
-      <div class="item_card_sold">(Sold out at 100%)</div>
     </div>
-    <div class="item_card_right">
-      <div class="item_right_heading">
-        <div class="item_term">
-          <div class="term_title">Contract Term</div>
-          <div class="term_content">
-            {{ item.contract_term }}
+    <n-modal v-model:show="showModal" transform-origin="center" style="margin: auto">
+      <n-card
+        style="width: 600px; min-width: 600px"
+        title="Checkout
+
+"
+        :bordered="false"
+        size="huge"
+        role="dialog"
+        aria-modal="true"
+      >
+        <template #header-extra>
+          <div @click="showModal = false" style="cursor: pointer">
+            <img alt="close" src="@/assets/images/x.svg" style="width: 20px" />
           </div>
-        </div>
-        <div class="item_term">
-          <div class="term_title">Contract Price</div>
-          <div class="term_content">
-            {{ item.contract_price }}
-          </div>
-        </div>
-      </div>
-      <div class="item_right_body">
-        <div class="item_term">
-          <div class="term_title">Daily Rewards</div>
-          <div class="term_content">
-            {{ item.daily_rewards }}
-          </div>
-        </div>
-      </div>
-      <div class="item_right_footer d-flex justify-content-start">
-        <router-link to="/sign-in">
-        <ButtonComponent text="Order Now" class="default_black" />
-        </router-link>
-        <router-link :to="'/product-detail/' + item.id">
-        <ButtonComponent text="See more" class="default_white ml-1" />
-        </router-link>
-      </div>
-    </div>
+        </template>
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolores
+        fugiat natus non quam sed. Alias commodi consectetur cumque ea, est ex excepturi facilis
+        fugit illo modi sapiente voluptates?
+        <template #footer> </template>
+      </n-card>
+    </n-modal>
   </div>
 </template>
 
 <script>
 import ButtonComponent from '@/components/mini_components/ButtonComponent.vue'
-
+import { NModal } from 'naive-ui'
+import { NCard } from 'naive-ui'
 export default {
   name: 'SingleMining',
-  components: { ButtonComponent },
+  components: { ButtonComponent, NModal, NCard },
   props: {
     item: Object
+  },
+  data() {
+    return {
+      showModal: false,
+      bodyStyle: {
+        width: '600px'
+      },
+      segmented: {
+        content: 'soft',
+        footer: 'soft'
+      }
+    }
   }
 }
 </script>
