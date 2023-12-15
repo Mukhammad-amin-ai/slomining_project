06024f90a386be20e3b350d9f40c7ade6026c965
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-15 21:33:00 +0500

    side bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template >
+  <modalSidebar />
   <headerComp />
+
   <div>
     <router-view></router-view>
   </div>
+
   <stickyNav />
   <footerComp />
 </template>
@@ -10,11 +13,14 @@
 import headerComp from "@/components/header/header-comp.vue"
 import footerComp from "./components/footer/footer-comp.vue";
 import stickyNav from "./components/header/sticky-nav.vue";
+import modalSidebar from "./components/header/modal-sidebar.vue";
+
 export default {
   components: {
     headerComp,
     footerComp,
-    stickyNav
+    stickyNav,
+    modalSidebar
   }
 }
 </script>
