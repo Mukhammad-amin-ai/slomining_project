9f3e4cf125d03a0f3dcee31faebaf3dedf13c7b3
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-15 09:09:44 +0500

    new

--- src/views/home-comp.vue
+++ src/views/home-comp.vue
@@ -13,7 +13,7 @@
       </div>
       <div class="hero-galery">
         <div class="galery-image">
-          <img src="../assets/images/hero.png" alt="" draggable="false" />
+          <img src="../assets/images/hero.png" alt="hero" draggable="false" />
         </div>
       </div>
     </div>
