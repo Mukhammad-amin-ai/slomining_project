b05bdd86d47283d432fb86457fac2165543108ea
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-12-15 13:53:55 +0500

    add to cart init

--- src/static/data.js
+++ src/static/data.js
@@ -6,7 +6,7 @@ const data = [
       'https://nft1-bucket.s3.amazonaws.com/user/1/4ffea657c35c494d8ad047c27dc3585d.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231213T114940Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIAYUAO3C7T6MBXPYJW%2F20231213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=4fac6045c3db5d481ace95bef1ef6ab1c6895c08550aa5c2c06ec5872d69de22',
     volume: 32,
     contract_term: '1 Day',
-    contract_price: '$100',
+    contract_price: 100,
     daily_rewards: '0.00004390 BTC ( ≈ $1.80 )',
     detail:
       '<p>1.Purchase Instructions:<br><br>A Mining Contract becomes active immediately after payment confirmation. Unless otherwise provided by applicable law or a specific offer, all purchases are final and non-refundable.<br><br><br>2.Calculation Output:<br><br>For the calculation of mining revenue, please refer to: <a href="https://slomining.com/contracts" target="_blank">https://slomining.com/contracts</a><br><br><br>3.Output Settlement:<br><br>The cloud machine will cease operation after the contract expires, and the system will settle the principal and profits for you. Only when the revenue reaches the amount specified by the platform can it be withdrawn (Minimum withdrawal amount is $50).<br><br><br>4.Termination of Contract:<br><br>The contract will be automatically terminated upon expiration. There is no refund for early termination. Thank you for your understanding.<br><br><br>5.Risk Tips:<br><br>Neither party shall be liable for power failures or damage to mines or mining machines caused by natural disasters (such as floods, debris flows, earthquakes, hurricanes, etc.), policy impacts (national publications and documents, etc.), war, political turmoil, and other force majeure events.<br><br>The platform reserves the right to provide the final interpretation of the terms of this contract.</p>'
@@ -18,7 +18,7 @@ const data = [
       'https://nft1-bucket.s3.amazonaws.com/user/1/276aae22747e42a4b9f12a3787774d9d.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231213T115050Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIAYUAO3C7T6MBXPYJW%2F20231213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=3f6dba1e05fe82a0efe604966534b2ab69c6937dd16ba2729dbe7015807b8d59',
     volume: 64,
     contract_term: '8 Days',
-    contract_price: '$1600',
+    contract_price: 1600,
     daily_rewards: '0.00085854 BTC ( ≈ $35.20 )',
     fixes_rewards: '$1600 + $281.60',
     detail:
@@ -31,7 +31,7 @@ const data = [
       'https://nft1-bucket.s3.amazonaws.com/user/1/ef43ab96468441eba4220eb58e52fe04.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231213T114948Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIAYUAO3C7T6MBXPYJW%2F20231213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=8cc52dd03df0609ae661651d8c41d6c3d4847ccfe67ab3915a2152072101b2e7',
     volume: 90,
     contract_term: '7 Days',
-    contract_price: '$500',
+    contract_price: 500,
     daily_rewards: '0.00024390 BTC ( ≈ $10.00 )',
     fixes_rewards: '$500 + $70.00',
     detail:
@@ -44,7 +44,7 @@ const data = [
       'https://nft1-bucket.s3.amazonaws.com/user/1/4ffea657c35c494d8ad047c27dc3585d.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231213T114940Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIAYUAO3C7T6MBXPYJW%2F20231213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=4fac6045c3db5d481ace95bef1ef6ab1c6895c08550aa5c2c06ec5872d69de22',
     volume: 32,
     contract_term: '1 Day',
-    contract_price: '$100',
+    contract_price: 100,
     daily_rewards: '0.00004390 BTC ( ≈ $1.80 )',
     detail:
       '<p>1.Purchase Instructions:<br><br>A Mining Contract becomes active immediately after payment confirmation. Unless otherwise provided by applicable law or a specific offer, all purchases are final and non-refundable.<br><br><br>2.Calculation Output:<br><br>For the calculation of mining revenue, please refer to: <a href="https://slomining.com/contracts" target="_blank">https://slomining.com/contracts</a><br><br><br>3.Output Settlement:<br><br>The cloud machine will cease operation after the contract expires, and the system will settle the principal and profits for you. Only when the revenue reaches the amount specified by the platform can it be withdrawn (Minimum withdrawal amount is $50).<br><br><br>4.Termination of Contract:<br><br>The contract will be automatically terminated upon expiration. There is no refund for early termination. Thank you for your understanding.<br><br><br>5.Risk Tips:<br><br>Neither party shall be liable for power failures or damage to mines or mining machines caused by natural disasters (such as floods, debris flows, earthquakes, hurricanes, etc.), policy impacts (national publications and documents, etc.), war, political turmoil, and other force majeure events.<br><br>The platform reserves the right to provide the final interpretation of the terms of this contract.</p>'
@@ -56,7 +56,7 @@ const data = [
       'https://nft1-bucket.s3.amazonaws.com/user/1/276aae22747e42a4b9f12a3787774d9d.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231213T115050Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIAYUAO3C7T6MBXPYJW%2F20231213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=3f6dba1e05fe82a0efe604966534b2ab69c6937dd16ba2729dbe7015807b8d59',
     volume: 64,
     contract_term: '8 Days',
-    contract_price: '$1600',
+    contract_price: 1600,
     daily_rewards: '0.00085854 BTC ( ≈ $35.20 )',
     fixes_rewards: '$1600 + $281.60',
     detail:
@@ -69,7 +69,7 @@ const data = [
       'https://nft1-bucket.s3.amazonaws.com/user/1/ef43ab96468441eba4220eb58e52fe04.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231213T114948Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIAYUAO3C7T6MBXPYJW%2F20231213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=8cc52dd03df0609ae661651d8c41d6c3d4847ccfe67ab3915a2152072101b2e7',
     volume: 90,
     contract_term: '9 Days',
-    contract_price: '$500',
+    contract_price: 500,
     daily_rewards: '0.00024390 BTC ( ≈ $10.00 )',
     fixes_rewards: '$1000 + $70.00',
     detail:
