59316ac25e22edd63f87d95a9be7635ef4308c14
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-16 18:15:37 +0500

    changes

--- src/static/data.js
+++ src/static/data.js
@@ -92,6 +92,18 @@ const data = [
     fixes_rewards: '$1000 + $70.00',
     detail:
       '<p>1.Purchase Instructions:<br><br>A Mining Contract becomes active immediately after payment confirmation. Unless otherwise provided by applicable law or a specific offer, all purchases are final and non-refundable.<br><br><br>2.Calculation Output:<br><br>For the calculation of mining revenue, please refer to: <a href="https://slomining.com/contracts" target="_blank">https://slomining.com/contracts</a><br><br><br>3.Output Settlement:<br><br>The cloud machine will cease operation after the contract expires, and the system will settle the principal and profits for you. Only when the revenue reaches the amount specified by the platform can it be withdrawn (Minimum withdrawal amount is $50).<br><br><br>4.Termination of Contract:<br><br>The contract will be automatically terminated upon expiration. There is no refund for early termination. Thank you for your understanding.<br><br><br>5.Risk Tips:<br><br>Neither party shall be liable for power failures or damage to mines or mining machines caused by natural disasters (such as floods, debris flows, earthquakes, hurricanes, etc.), policy impacts (national publications and documents, etc.), war, political turmoil, and other force majeure events.<br><br>The platform reserves the right to provide the final interpretation of the terms of this contract.</p>'
+  },
+  {
+    id: 6,
+    name: 'Ethereum #2316',
+    image: '/card/9.jpg',
+    volume: 90,
+    contract_term: '9 Days',
+    contract_price: 500,
+    daily_rewards: '0.00024390 BTC ( ≈ $10.00 )',
+    fixes_rewards: '$1000 + $70.00',
+    detail:
+      '<p>1.Purchase Instructions:<br><br>A Mining Contract becomes active immediately after payment confirmation. Unless otherwise provided by applicable law or a specific offer, all purchases are final and non-refundable.<br><br><br>2.Calculation Output:<br><br>For the calculation of mining revenue, please refer to: <a href="https://slomining.com/contracts" target="_blank">https://slomining.com/contracts</a><br><br><br>3.Output Settlement:<br><br>The cloud machine will cease operation after the contract expires, and the system will settle the principal and profits for you. Only when the revenue reaches the amount specified by the platform can it be withdrawn (Minimum withdrawal amount is $50).<br><br><br>4.Termination of Contract:<br><br>The contract will be automatically terminated upon expiration. There is no refund for early termination. Thank you for your understanding.<br><br><br>5.Risk Tips:<br><br>Neither party shall be liable for power failures or damage to mines or mining machines caused by natural disasters (such as floods, debris flows, earthquakes, hurricanes, etc.), policy impacts (national publications and documents, etc.), war, political turmoil, and other force majeure events.<br><br>The platform reserves the right to provide the final interpretation of the terms of this contract.</p>'
   }
 ]
 export default data
