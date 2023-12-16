700dd3cfa27ac8647e3e81455cedbe49af5d07d9
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-16 10:05:50 +0500

    dashboard profile

--- src/components/dashboard/ProfileComponent.vue
+++ src/components/dashboard/ProfileComponent.vue
@@ -29,6 +29,20 @@
           <h5>$ 0.00</h5>
         </div>
       </div>
+      <div class="toptal-resp">
+        <div class="item">
+          <h3>Total Deposit</h3>
+          <h5>$ 0.00</h5>
+        </div>
+        <div class="item">
+          <h3>Total Withdrawal</h3>
+          <h5>$ 0.00</h5>
+        </div>
+        <div class="item">
+          <h3>Total Earned</h3>
+          <h5>$ 0.00</h5>
+        </div>
+      </div>
       <div class="profit d-flex">
         <div class="item">
           <h3>Profits</h3>
@@ -140,6 +154,13 @@ h5 {
 
 }
 
+.toptal-resp {
+  width: 100%;
+  height: auto;
+  display: none;
+  gap: 10px;
+}
+
 .profit {
   width: 100%;
   height: 120px;
@@ -153,4 +174,82 @@ h5 {
   border-radius: 0.5rem;
   padding: 12px 22px;
 }
+
+@media screen and (max-width:1380px) {
+  .contain {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width:1024px) {
+  .dashboard-profile {
+    width: 90%;
+  }
+
+  .contain {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width:768px) {
+  .total {
+    display: none !important;
+  }
+
+  .toptal-resp {
+    display: flex;
+  }
+
+  .toptal-resp .item {
+    background-color: #fff;
+    box-shadow: 0 2px 6px 0 rgba(0, 0, 0, .08);
+    border-radius: 0.5rem;
+    padding: 20px;
+  }
+
+  .item {
+    border: 1px solid #000;
+  }
+
+}
+
+@media screen and (max-width:600px) {
+  .contain {
+    height: auto;
+  }
+
+  .toptal-resp {
+    flex-direction: column;
+  }
+
+  .profit {
+    height: auto;
+    flex-direction: column;
+    gap: 0;
+  }
+
+  .profit .item {
+    padding: 20px;
+  }
+
+  .item {
+    width: 100%;
+  }
+  .dashboard-profile{
+    margin: 10px 0 0 10px !important;
+  }
+}
+
+@media screen and (max-width:425px) {
+  .balance{
+    height: auto;
+    flex-direction: column;
+    align-items: flex-start !important;
+  }
+  .buttns{
+    gap: 10px;
+  }
+}
+
+
 </style>
