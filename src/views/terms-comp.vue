151720548e72f192b479c6d32fe37b12087200ed
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-17 13:48:31 +0500

    terms

--- src/views/terms-comp.vue
+++ src/views/terms-comp.vue
@@ -7,175 +7,98 @@
             <section class="container">
                 <div class="list-header">
                     <h5>
-                        Last Updated: 5 July 2021
+                        Skyhash User Agreement
                     </h5>
                     <br>
                     <p>
-                        This agreement is made between you as a user and platform SLOMINING. <br>
+                        This agreement establishes the terms between you, as the user, and the platform Skyhash. <br>
                         <br>
-                        Using the services of the site SLOMINING, you agree that you have read and accepted all the
-                        conditions described in this agreement, as well as familiarized with our Privacy Policy. <br>
+                        By utilizing Skyhash's services, you confirm your acknowledgment and acceptance of all conditions
+                        outlined in this agreement, along with a thorough understanding of our Privacy Policy.<br>
                         <br>
-                        If you do not agree with this agreement, immediately stop using the services of the platform
-                        SLOMINING. <br>
+                        Should you disagree with these terms, kindly cease using Skyhash's platform immediately. <br>
                         <br>
-                        By creating your account at SLOMINING, you consent to the terms and conditions set forth in this
+                        Upon creating your Skyhash account, you implicitly consent to the stipulations presented in this
                         User Agreement. <br>
                         <br>
-                        If you have any related questions, you can always contact our support team.
+                        For any queries related to this agreement, our support team is readily available to assist you.
                     </p>
                 </div>
                 <h4>Terms and Definitions </h4>
                 <ul>
-                    <li> 1. Agreement is a list of the conditions of Users that must be accepted to continue working on the
-                        platform SLOMINING.</li>
-                    <li>2. Cryptocurrency - a peer-to-peer electronic currency that does not have a single centralized
-                        issuer, which is distributed between holders of such a currency.</li>
-                    <li>3. A personal account is a page inside the platform that is created as a result of registration and
-                        acceptance of the User Agreement.</li>
-                    <li>4. Privacy policy - these are the rules for the collection, storage and use of personal data of
-                        users.</li>
-                    <li>5. User is a natural or legal person registered in accordance with local national legislation,
-                        registered and accepted the User Agreement.</li>
-                    <li>6. Deposit and withdrawal of funds - an operation to transfer the user's funds from a personal
+                    <li> 1. Agreement - The set of conditions users must accept to continue operating on the Skyhash
+                        platform.</li>
+                    <li>2. Cryptocurrency - A decentralized electronic currency distributed among its holders.</li>
+                    <li>3. Personal Accoun - A user's page within the platform, generated through registration and
+                        acceptance of the User Agreement</li>
+                    <li>4. Privacy policy - Rules governing the collection, storage, and utilization of users' personal
+                        data.</li>
+                    <li>5.User - An individual or legal entity registered and accepting the User Agreement in accordance
+                        with
+                        local national legislation.</li>
+                    <li>6. Deposit and Withdrawal of Funds - an operation to transfer the user's funds from a personal
                         account.</li>
-                    <li>7. Account data - personal data of the user, this is the username, password, email address that the
-                        user specified when registering on the platform SLOMINING.</li>
-
+                    <li>7. Account data - User's personal information, including username, password, and email address
+                        provided during platform registration.</li>
                 </ul>
-                <h4>Terms of agreement </h4>
-                <p>About such changes SLOMINING will notify all users by the specified mail by the user, and all updates
-                    will be displayed in the Agreement Terms themselves.
-
-                    If you continue to work on the platform, then you automatically accept these updates.
-
-                    By creating an account in SLOMINING, you confirm that you are at least 18 years old and you are an
-                    individual or legal entity, an organization that is fully authorized to accept this User Agreement
-                    between you and SLOMINING.</p>
-
-                <h4>Prohibition of use </h4>
-                <p>Using the services of the platform SLOMINING, you declare that you are not on any list of trade or
-                    economic sanctions, such as the List of Sanctions of the UN Security Council and the like. <br>
-
-                    This Agreement cannot be excluded under the laws of the country in which the user is located.</p>
-
-                <h4>Description of the services </h4>
-                <p>Users have the opportunity to register, open accounts and deposit assets in SLOMINING. Platform users
-                    have the right to request the withdrawal of their funds, taking into account the restrictions specified
-                    in the Terms of the agreement, if any. <br>
-
-                    All users should be aware of all the risks associated with all possible movements of cryptocurrency
-                    rates. <br>
-
-                    SLOMINING never requires transfers of funds to the wallet and never asks for your account password. <br>
-
-                    Users need to be careful about promotions and any discounts that could lead to fraud and loss of funds.
-                    All notifications that come to the e-mail specified by users will never require any transaction data,
-                    personal data until a user requests it. <br>
-
-                    The list of fraudulent transactions is wider, the user agrees that SLOMINING does not take
-                    responsibility for any loss of funds resulting from the situations listed above. <br>
-
-                    By using the services of SLOMINING, you confirm that all information provided by SLOMINING under this
-                    Agreement is true, complete and accurate.</p>
-
-                <h4>Registration and account requirements </h4>
-                <p>Before starting work on the platform SLOMINING all users must go through a simple registration. <br>
-
-                    And also users need to accept the Terms of Use, Privacy Policy. <br>
-
-                    To create an account you need to provide your real name, email address and password. <br>
-
-                    At our sole discretion and depending on a number of conditions, SLOMINING may refuse to open an account.
+                <h4>Terms of Agreement </h4>
+                <p>Skyhash will notify users of any changes via the specified email, with updates displayed within the
+                    Agreement Terms. Continuing to use the platform implies automatic acceptance of these updates.
                     <br>
+                    By creating a Skyhash account, you affirm that you are at least 18 years old and are authorized to
+                    accept this User Agreement on behalf of yourself or your organization.
                 </p>
-                <h4>User data authentication</h4>
-                <p>By creating an account with SLOMINING, you agree to provide personal data to verify the authenticity of
-                    your identity. <br>
-
-                    These measures are necessary to detect and prevent schemes of money laundering, fraud, financing of
-                    terrorist organizations and other financial crimes. <br>
-
-                    After registration, you must ensure that your information provided is complete, truthful, and you will
-                    be updated in a timely manner in the event of a change. <br>
-
-                    If there is any doubt that the information you submitted is untrue or is outdated or incomplete,
-                    SLOMINING has the right to send you a notification asking you to correct or update personal data. <br>
-
-                    You take full responsibility for any losses or expenses incurred at the time of use SLOMINING if you
-                    cannot contact in a timely manner and update your personal information.
-
-                    By this agreement, you acknowledge that you undertake to update personal information in case of any
-                    changes.</p>
-
-                <h4>Account Requirements </h4>
-                <p>SLOMINING has the right to freeze or cancel accounts used by third parties that are not account holders.
-                    <br>
-
-                    SLOMINING is not responsible for these accounts. <br>
+                <h4>Prohibition of Use</h4>
+                <p>Users of Skyhash declare that they are not on any trade or economic sanctions lists. This Agreement
+                    cannot be excluded under the laws of the user's country. <br>
                 </p>
+                <h4>Description of Services </h4>
+                <p>Skyhash allows users to register, open accounts, and deposit assets. Users can request fund withdrawals
+                    within specified terms, understanding the associated risks of cryptocurrency rate fluctuations <br>
 
-                <h4>Account security</h4>
-                <p>SLOMINING, strives to create maximum security to preserve user funds. <br>
-
-                    You are solely responsible for maintaining the security of your account at SLOMINING, for maintaining
-                    the password, and the electronic login address. <br>
-
-                    Our company asks you, as account holders, to also be extremely careful and take precautions to protect
-                    your account and personal information. <br>
-
-                    You will comply with the security system, authentication, processing and collection of payments or
-                    withdrawals. you immediately inform SLOMINING if you become aware of any unauthorized use of your
-                    account and password in SLOMINING by another person, or other violations of security rules. SLOMINING
-                    does not take responsibility for any losses and consequences caused by your neglect of the above account
-                    security statement.</p>
-
-                <h4>Copyright and trademarks</h4>
-                <p>In order to properly use Intellectual Property SLOMINING, obtaining the necessary permissions is
-                    required.</p>
-                <h4>Recommendations for use</h4>
-                <p>You hereby agree to abide by these points when you use the platform SLOMINING: <br>
-
-                    will not shy away from paying taxes or government fees. all the actions you take while working on
-                    SLOMINING will meet the requirements of applicable laws, as well as charters SLOMINING. this agreement
-                    will not be violated, and the rules SLOMINING. If you have violated the above paragraphs and this has
-                    led to any legal consequences, you will independently carry out all legal paragraphs on your own behalf
-                    and protect SLOMINING from all claims and costs associated with such a violation. <br>
-
-                    You will use the platform in accordance with the Privacy Policy and Terms of Use, without interfering
-                    with the normal operation of the platform SLOMINING.</p>
-                <h4>Limitation of liability</h4>
-                <p>SLOMINING is not responsible for any of your losses resulting from the events described below, but not
-                    limited to: <br>
-
-                    unauthorized alteration of your data by third parties. loss of profit, goodwill or other intangible
-                    losses. unauthorized use of your account. other losses.</p>
+                    Skyhash never requests fund transfers to wallets or account passwords. Users are urged to exercise
+                    caution regarding promotions and discounts to avoid fraud and fund loss.<br>
+                </p>
+                <h4>Registration and Account Requirements</h4>
+                <p>Before commencing activities on the Skyhash platform, users must undergo a simple registration process
+                    and accept the Terms of Use and Privacy Policy. Account creation involves providing real names, email
+                    addresses, and passwords. Skyhash reserves the right to refuse account creation at its discretion.
+                </p>
+                <h4>User Data Authentication</h4>
+                <p>Users agree to provide personal data for identity verification. This is crucial to detect and prevent
+                    money laundering, fraud, and other financial crimes. Users must ensure timely updates to their
+                    information.</p>
+                <h4>Account Requirements</h4>
+                <p>Skyhash has the right to freeze or cancel accounts used by third parties not registered as account
+                    holders. Skyhash is not responsible for these accounts. <br>
+                </p>
+                <h4>Account Security</h4>
+                <p>Users are responsible for maintaining account security, including passwords and electronic login
+                    addresses. Users must promptly report any unauthorized use to Skyhash, which is not liable for losses
+                    due to neglect of account security.</p>
+                <h4>Copyright and Trademarks</h4>
+                <p>Permission is required to use Skyhash's intellectual property.</p>
+                <h4>Recommendations for Use</h4>
+                <p>Users agree to comply with tax obligations, legal requirements, and Skyhash's rules while using the
+                    platform. Users are responsible for legal consequences resulting from violations.</p>
+                <h4>Limitation of Liability</h4>
+                <p>Skyhash is not liable for losses resulting from unauthorized data alterations, loss of profit,
+                    unauthorized account use, or other losses.</p>
                 <h4>Recompense</h4>
-                <p>You agree to protect SLOMINING, its current employees, directors, affiliates and agents from all kinds of
-                    losses incurred by a third party as a result of User SLOMINING actions in violation of this Agreement.
+                <p>Users agree to protect Skyhash from losses incurred by third parties due to user actions violating this
+                    Agreement.
                 </p>
                 <h4>Termination</h4>
-                <p>You agree that SLOMINING has the right to stop your use of the platform, block funds if it is suspected
-                    that such accounts have violated the Terms of the Agreement, the Privacy Policy, or other applicable
-                    legislation. SLOMINING, has the right to save, use and provide transaction data of violators at the
-                    request of the legislature. <br>
-
-                    The termination of the agreement is possible with such factors: <br>
-
-                    by order of a court or other regulatory authority. if unauthorized access to the account is detected. if
-                    non-natural account activity is detected. After the account is closed and the funds are blocked, the
-                    entire balance of the user will go to pay off all costs and expenses SLOMINING.</p>
-                <h4>Privacy policy</h4>
-                <p>Privacy policy is an important clause of this agreement. SLOMINING may, if necessary, amend this clause
-                    of the agreement. All changes to the Privacy Policy, and any other clause, will be notified to all users
-                    through the mailing list indicated by their email.</p>
+                <p>Skyhash can terminate user accounts suspected of violating the Agreement, Privacy Policy, or applicable
+                    laws. Termination factors include court orders, unauthorized account access, and non-natural account
+                    activity.</p>
+                <h4>Privacy Policy</h4>
+                <p>Changes to the Privacy Policy will be communicated to users through the specified email.</p>
                 <h4>Compensation and Disclaimer</h4>
-                <p>By accepting this agreement, the user agrees to reimburse all losses and expenses incurred SLOMINING if
-                    the user's actions led to such losses. <br>
-
-                    SLOMINING is not responsible for errors and inaccuracies associated with the use of its services.</p>
-                <h4>Support service</h4>
-                <p>If you have any questions about these terms, please contact info@slomining.com</p>
+                <p>Users agree to reimburse Skyhash for losses resulting from their actions. Skyhash is not responsible for
+                    errors associated with its services.</p>
+                <h4>Support Service</h4>
+                <p>For inquiries about these terms, contact info@skyhash.com.</p>
             </section>
         </div>
     </div>
@@ -217,11 +140,13 @@ h2 {
     height: auto;
     padding: 20px 0;
 }
+
 .list-header h5 {
     font-family: Montserrat-Light, sans-serif;
     color: var(--blue);
     font-size: 17px;
 }
+
 .list-header p {
     padding: 20px 0 0 0;
 }
@@ -254,6 +179,4 @@ li {
     font-size: 16px;
     line-height: 26px;
 }
-
-
 </style>
